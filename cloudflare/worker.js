916ee22bd234dc7156d088fe108a537fb454c79d
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2025-11-25 19:15:22 -0800

    Add worker entry point for API lookups

--- cloudflare/worker.js
+++ cloudflare/worker.js
@@ -1,26 +1,46 @@
-export default {
-    // Function for getting a request from the cache or going to origin.
-  async fetchCacheOrOrigin(request, ctx) {
-    // Check if data is in cache
-    const cacheUrl = new URL(request.url);
-    const cacheKey = new Request(cacheUrl.toString());
-    const cache = caches.default;
-    let response = await cache.match(cacheKey);
-    // Data is not in the cache already
-    if (!response) {
-      // Fetch the request.
-      response = await fetch(request);
-      console.log(`Fetching Origin for Cache ${request.url}`);
-      // Cache it
-      response = new Response(response.body, response);
-      ctx.waitUntil(cache.put(cacheKey, response.clone()));
+import { WorkerEntrypoint } from "cloudflare:workers";
+
+// Function for getting a request from the cache or going to origin.
+async function fetchCacheOrOrigin(request, ctx) {
+  // Check if data is in cache
+  const cacheUrl = typeof request == 'string' ? new URL(request) : new URL(request.url);
+  const cacheKey = new Request(cacheUrl.toString());
+  const cache = caches.default;
+  let response = await cache.match(cacheKey);
+  // Data is not in the cache already
+  if (!response) {
+    // Fetch the request.
+    response = await fetch(request);
+    console.log(`Fetching Origin for Cache ${cacheUrl.toString()}`);
+    // Cache it
+    response = new Response(response.body, response);
+    ctx.waitUntil(cache.put(cacheKey, response.clone()));
+  }
+  else
+    console.log(`Data was in cache already for ${cacheUrl.toString()}`);
+
+  return response;
+};
+
+export class APILookup extends WorkerEntrypoint {
+  async checkAccount(account) {
+    if (account == 0) {
+      return {};
     }
-    else
-      console.log(`Data was in cache already for ${request.url}`);
+    return await fetchCacheOrOrigin(`https://api.scamguard.app/check/${account}`);
+  };
+  async getBanDetails(account) {
+    if (account == 0) {
+      return {};
+    }
+    return await fetchCacheOrOrigin(`https://api.scamguard.app/ban/${account}`);
+  };
+  async getBanStats() {
+    return await fetchCacheOrOrigin(`https://api.scamguard.app/bans`);
+  }
+};
 
-    return response;
-  },
-
+export default {
   async fetch(request, env, ctx) {
     // Check if authorization is enabled
     const authRequired = (env.REQUIRE_AUTH === "true");
