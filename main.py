2c37a6dd35e6400bf825dc3b1a2bcd2f7dc3f9c4
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-02-21 19:20:33 -0800

    update api docs
    and the output of everything too!

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse, RedirectResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse, RedirectResponse, JSONResponse
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
 from typing import Union
 from DatabaseDriver import DatabaseDriver
 from DatabaseSchema import Ban
@@ -28,18 +31,20 @@ If you would like to obtain an API Token, please send a message in the `#api-req
 """
 
 
-app = FastAPI(redoc_url=None, openapi_url="/openapi.json", description=global_description, title=global_title, summary=global_summary,
+app = FastAPI(redoc_url=None, docs_url=None, openapi_url="/openapi.json", description=global_description, title=global_title, summary=global_summary,
               contact={"name":"Support Contact", "url":contact_url}, terms_of_service=f"https://{host_name}/terms",
               license_info={"name":"MIT", "url":"https://github.com/theantiscamgroup/AntiScamBotAPI/blob/main/LICENSE"},
               servers=[{"url": f"https://{api_host_name}", "description": "Production API"}], version=global_version)
 
 db = DatabaseDriver()
 
-class APIBan(BaseModel):
+class BaseAPIResponse(BaseModel):
+  valid: bool = False
+
+class APIBan(BaseAPIResponse):
   banned: bool = False
   user_id: int = 0
   user_id_str: str = ""
-  valid: bool = False
 
   def Create(self, user_id:int=0):
     self.user_id = user_id
@@ -80,27 +85,43 @@ class APIBanDetailed(APIBan):
 
     return self
 
-class APIStats(BaseModel):
-  count:int = 0
-  valid:bool = True
+class APIStats(BaseAPIResponse):
+  count: int = 0
 
   def Execute(self):
+    self.valid = True
     self.count = db.GetNumBans()
     return self
 
-class APIAuthError(BaseModel):
-  valid: bool = False
+class APIAuthError(BaseAPIResponse):
   msg: str = "Invalid Auth Key Provided"
+
+class APIInvalidData(BaseAPIResponse):
+  msg: str = "Provided data was invalid"
+  details: Union[str, None] = None
+
+  def Execute(self, data: str):
+    self.details = data
+    return self
 
 @app.get("/", include_in_schema=False, response_class=RedirectResponse, status_code=302)
 def main():
   return f"https://{api_host_name}/docs"
 
-@app.get("/check/{user_id}", description="Check if a Discord UserID is banned", response_model=APIBan, responses={403: {"model": APIAuthError}})
+@app.get("/docs", include_in_schema=False)
+async def docs_output(req: Request):
+  return get_swagger_ui_html(
+      openapi_url=app.openapi_url,
+      title=app.title,
+      swagger_favicon_url="/favicon.png",
+      swagger_ui_parameters=app.swagger_ui_parameters,
+  )
+
+@app.get("/check/{user_id}", description="Check if a Discord UserID is banned", response_model=APIBan, responses={403: {"model": APIAuthError}, 422: {"model": APIInvalidData}})
 def check_ban(user_id: int):
   return APIBan().Create(user_id).Execute()
 
-@app.get("/ban/{user_id}", description="Get extensive information as to an UserID being banned", response_model=APIBanDetailed, responses={403: {"model": APIAuthError}})
+@app.get("/ban/{user_id}", description="Get extensive information as to an UserID being banned", response_model=APIBanDetailed, responses={403: {"model": APIAuthError}, 422: {"model": APIInvalidData}})
 def get_ban_info(user_id: int):
   return APIBanDetailed().Create(user_id).Execute()
 
@@ -108,6 +129,19 @@ def get_ban_info(user_id: int):
 def get_ban_stats():
    return APIStats().Execute()
 
-@app.get('/favicon.ico', include_in_schema=False)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc: RequestValidationError):
+  message = ""
+  for error in exc.errors():
+      message += f"[Field: {error['loc']}, Error: {error['msg']}] "
+  ErrObj = jsonable_encoder(APIInvalidData().Execute(message))
+  return JSONResponse(content=ErrObj, status_code=422)
+
+@app.get('/favicon.png', include_in_schema=False)
 async def favicon():
-  return FileResponse("favicon.ico")
+  return FileResponse("favicon.png")
+
+@app.get('/openapi.json', include_in_schema=False)
+async def openapi():
+  return FileResponse("openapi.json")
