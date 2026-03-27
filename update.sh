d32e5cd2342ac4234546d6a2f33d0810fe1d19c8
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-03-26 23:06:09 -0700

    ok this better work

---
+++ update.sh
@@ -0,0 +1,24 @@
+gitpull() {
+  if git pull; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+echo "Setting SSH Agent"
+eval "$(ssh-agent -s)"
+echo "Readding SSH Key"
+ssh-add ~/.ssh/scamguardapi
+echo "Checking Git Readability"
+ssh -T git@github.com
+echo "Updating directory"
+if gitpull; then
+  echo "Deploy success"
+else
+  echo "Stashing changes!"
+  git stash
+  echo "Resyncing..."
+  gitpull
+  echo "Remember to handle the unstashing later"
+fi
