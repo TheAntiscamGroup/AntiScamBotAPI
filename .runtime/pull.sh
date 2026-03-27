b23a845711592f63d74584ea98bcaa18ea3ab58c
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-03-26 23:27:02 -0700

    update scripts again

--- .runtime/pull.sh
+++ .runtime/pull.sh
@@ -6,12 +6,13 @@ gitpull() {
   fi
 }
 
-echo "Setting SSH Agent"
-eval "$(ssh-agent -s)"
-echo "Readding SSH Key"
-ssh-add ~/.ssh/scamguardapi
-echo "Checking Git Readability"
-ssh -T git@github.com
+#echo "Setting SSH Agent"
+#eval "$(ssh-agent -s)"
+#echo "Readding SSH Key"
+#ssh-add ~/.ssh/scamguardapi.pub
+#echo "Checking Git Readability"
+#ssh -T git@github.com
+cd ..
 echo "Updating directory"
 if gitpull; then
   echo "Deploy success"
